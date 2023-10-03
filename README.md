0a8e82da54a2fc8ada4d66e8089529b46f2b55bb
Brett Harrison <32527754+normon66@users.noreply.github.com>
2023-10-02 20:03:54 -0400

    create custom tester feedback form based on GithubFeedbackForm package

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A docassemble extension.
 
 ## Author
 
+9/29/23    MLH custom tester feedback form. bharrison
 8/21/23    Incorporate standard intro/outro screens, global variables. Brett Harrison
 Dec 2022   NEW. Emily K. Miller, ekressmiller@lsscm.org
 
--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.mlhframework',
-      version='0.1.8',
+      version='0.1.9',
       description=('A docassemble extension.'),
-      long_description='# docassemble.mlhframework\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\n8/21/23    Incorporate standard intro/outro screens, global variables. Brett Harrison\r\nDec 2022   NEW. Emily K. Miller, ekressmiller@lsscm.org\r\n\r\n',
+      long_description='# docassemble.mlhframework\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\n9/29/23    MLH custom tester feedback form. bharrison\r\n8/21/23    Incorporate standard intro/outro screens, global variables. Brett Harrison\r\nDec 2022   NEW. Emily K. Miller, ekressmiller@lsscm.org\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Emily K. Miller',
       author_email='ekressmiller@lsscm.org',
